ae89cc7cb8b2a8e17f5b2d4aece356cea4d697de
Frederic Espiau <frederic.espiau@gmail.com>
2021-06-27 14:35:20 +0200

    bump eslint and ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,21 @@ module.exports = {
       tsconfigRootDir: __dirname,
       project: ["./tsconfig.json"],
     },
-    plugins: ["@typescript-eslint", "jest", "unicorn"],
+    plugins: ["@typescript-eslint", "jest", "unicorn", "jsx-a11y"],
     extends: [
       "airbnb-typescript",
       "plugin:@typescript-eslint/recommended-requiring-type-checking",
       "plugin:jest/recommended",
       "plugin:unicorn/recommended",
+      "plugin:jsx-a11y/recommended",
       "prettier",
     ],
+    rules: {
+      "unicorn/prevent-abbreviations": 0,
+      "react/react-in-jsx-scope": 0,
+      "unicorn/filename-case": 0,
+      "import/prefer-default-export": 0,
+      "react/jsx-props-no-spreading": 0,
+      "unicorn/no-null": 0,
+    },
   };
