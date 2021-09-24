8eebfd1cce347411fc202d1f9d54b943ec23d3f9
Frederic Espiau <frederic.espiau@gmail.com>
2021-09-24 14:01:34 +0200

    bump ESlint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,59 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      tsconfigRootDir: __dirname,
-      project: ["./tsconfig.json"],
-    },
-    plugins: ["@typescript-eslint", "jest", "jsx-a11y"],
-    extends: [
-      "airbnb",
-      "airbnb-typescript",
-      "airbnb/hooks",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      "plugin:jest/recommended",
-      "plugin:jsx-a11y/recommended",
-      "plugin:import/typescript",
-      "prettier",
-    ],
-    rules: {
-      "react/react-in-jsx-scope": 0,
-      "import/prefer-default-export": 0,
-      "react/jsx-props-no-spreading": 0,
-      "import/extensions": 0,
-      "import/order": [
-        "error",
-        { alphabetize: { order: "asc", caseInsensitive: true } },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "jest", "node"],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/typescript",
+    "plugin:jest/recommended",
+    "plugin:node/recommended",
+    "prettier",
+  ],
+  env: {
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    node: {
+      resolvePaths: ["node_modules/@types"],
+      tryExtensions: [
+        ".cjs",
+        ".d.ts",
+        ".mjs",
+        ".node",
+        ".js",
+        ".json",
+        ".ts",
       ],
     },
-  };
+  },
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "off",
+    "import/order": [
+      "error",
+      { alphabetize: { order: "asc", caseInsensitive: true } },
+    ],
+    "import/prefer-default-export": "off",
+    "no-use-before-define": "off",
+    "no-void": "off",
+    "node/no-extraneous-import": "off",
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
+    ],
+  },
+};
