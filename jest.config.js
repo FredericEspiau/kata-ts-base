c82d79eef24d4b5cfbdc21939538168824d2eb5b
Frederic Espiau <frederic.espiau@gmail.com>
2021-11-13 09:51:44 +0100

    add ts-auto-mock

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-	moduleFileExtensions: ['jsx', 'js', 'json', 'ts', 'tsx'],
-	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-	transform: {
-		'^.+\\.(t|j)s$': 'ts-jest'
-	},
+  moduleFileExtensions: ["jsx", "js", "json", "ts", "tsx"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  setupFiles: ["<rootDir>config.ts"],
 };
